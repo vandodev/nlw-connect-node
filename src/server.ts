4b32d1fc836fda5ab466de78452846924874a5e3
vandodev <vandoaparecido@hotmail.com>
2025-03-13 07:06:35 -0300

    :rocket: :dollar: :white_check_mark: Documentação com fastifySwagger

--- src/server.ts
+++ src/server.ts
@@ -2,10 +2,13 @@ import { fastify } from 'fastify'
 import { fastifyCors } from '@fastify/cors'
 import {
     type ZodTypeProvider,
+    jsonSchemaTransform,
     serializerCompiler,
     validatorCompiler,
   } from 'fastify-type-provider-zod'
-  import z from 'zod'
+import z from 'zod'
+import fastifySwagger from '@fastify/swagger'
+import fastifySwaggerUi from '@fastify/swagger-ui'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
  
@@ -13,9 +16,24 @@ app.setSerializerCompiler(serializerCompiler)
 app.setValidatorCompiler(validatorCompiler)
 
 app.register(fastifyCors)
+app.register(fastifySwagger, {
+    openapi:{
+        info:{
+            title: 'nlw conect',
+            version: '0.0.1'
+        },
+    },
+    transform: jsonSchemaTransform
+})
+
+app.register(fastifySwaggerUi,{
+    routePrefix: '/docs'
+})
 
 app.post('/subscriptions',{
     schema:{
+        summary: 'Subscribes someone to the event',
+        tags: ['subscription'],
         body: z.object({
             name: z.string(),
             email: z.string().email()
