6cf547dd70e7fa34afc94478fc0de5deb06373c8
vandodev <vandoaparecido@hotmail.com>
2025-03-13 07:49:51 -0300

    :rocket: :dollar: :white_check_mark: Separando rotas

--- src/server.ts
+++ src/server.ts
@@ -6,9 +6,9 @@ import {
     serializerCompiler,
     validatorCompiler,
   } from 'fastify-type-provider-zod'
-import z from 'zod'
 import fastifySwagger from '@fastify/swagger'
 import fastifySwaggerUi from '@fastify/swagger-ui'
+import { subscribeToEventRoute } from './routes/subscribe-to-event-route'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
  
@@ -30,32 +30,7 @@ app.register(fastifySwaggerUi,{
     routePrefix: '/docs'
 })
 
-app.post('/subscriptions',{
-    schema:{
-        summary: 'Subscribes someone to the event',
-        tags: ['subscription'],
-        body: z.object({
-            name: z.string(),
-            email: z.string().email()
-        }),
-        response:{
-            201: z.object({
-                name: z.string(),
-                email: z.string()
-            })
-        }
-    }
-}, async (request, reply) =>{
-    const {name, email} = request.body
-
-    //Criação da inscrição no banco de dados
-
-    return reply.status(201).send({
-        name,
-        email
-    })
-})
-
+app.register(subscribeToEventRoute)
 
 app.listen({ port: 3333 }).then(() => {
     console.log('HTTP server running!')
