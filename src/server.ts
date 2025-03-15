8a1d3835ab0ba46e1ccdda8116944dcaed54bf11
vandodev <vandoaparecido@hotmail.com>
2025-03-15 07:43:36 -0300

    :rocket: :dollar: :white_check_mark: Contabilizando visitas e salvando no redis

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,16 @@
-import { fastify } from "fastify";
 import { fastifyCors } from "@fastify/cors";
+import fastifySwagger from "@fastify/swagger";
+import fastifySwaggerUi from "@fastify/swagger-ui";
+import { fastify } from "fastify";
 import {
 	type ZodTypeProvider,
 	jsonSchemaTransform,
 	serializerCompiler,
 	validatorCompiler,
 } from "fastify-type-provider-zod";
-import fastifySwagger from "@fastify/swagger";
-import fastifySwaggerUi from "@fastify/swagger-ui";
-import { subscribeToEventRoute } from "./routes/subscribe-to-event-route";
 import { env } from "./env";
+import { accessInviteLinkRoute } from "./routes/access-invite-link";
+import { subscribeToEventRoute } from "./routes/subscribe-to-event-route";
 
 const app = fastify().withTypeProvider<ZodTypeProvider>();
 
@@ -32,6 +33,7 @@ app.register(fastifySwaggerUi, {
 });
 
 app.register(subscribeToEventRoute);
+app.register(accessInviteLinkRoute);
 
 app.listen({ port: env.PORT }).then(() => {
 	console.log("HTTP server running!");
