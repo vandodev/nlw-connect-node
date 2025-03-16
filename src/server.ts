51c98007b089bcf624a469a8564cb2b205dc264f
vandodev <vandoaparecido@hotmail.com>
2025-03-16 07:45:01 -0300

    :rocket: :dollar: :white_check_mark: Ajustes

--- src/server.ts
+++ src/server.ts
@@ -9,7 +9,7 @@ import {
 	validatorCompiler,
 } from "fastify-type-provider-zod";
 import { env } from "./env";
-import { accessInviteLinkRoute } from "./routes/access-invite-link";
+import { accessInviteLinkRoute } from "./routes/access-invite-link-route";
 import { getSubscriberInviteClicksRoute } from "./routes/get-subscriber-invite-clicks-route";
 import { subscribeToEventRoute } from "./routes/subscribe-to-event-route";
 
