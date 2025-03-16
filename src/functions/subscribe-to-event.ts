0d7d80e80d00d2b8a8b9dbbd54478b669f502c71
vandodev <vandoaparecido@hotmail.com>
2025-03-16 08:25:08 -0300

    :rocket: :dollar: :white_check_mark: Adicionando indicação

--- src/functions/subscribe-to-event.ts
+++ src/functions/subscribe-to-event.ts
@@ -1,15 +1,18 @@
 import { eq } from "drizzle-orm";
 import { db } from "../drizzle/client";
 import { subscriptions } from "../drizzle/schema/subscriptions";
+import { redis } from "../redis/client";
 
 interface SubscribeToEventParams {
 	name: string;
 	email: string;
+	referrerId?: string | null;
 }
 
 export async function subscribeToEvent({
 	name,
 	email,
+	referrerId,
 }: SubscribeToEventParams) {
 	const subscribers = await db
 		.select()
@@ -31,6 +34,10 @@ export async function subscribeToEvent({
 		.returning();
 
 	const subscriber = result[0];
+
+	if (referrerId) {
+		await redis.zincrby("referral:ranking", 1, referrerId);
+	}
 
 	return {
 		subscriberId: subscriber.id,
