f78a70808805d99bf8fe24718fcddf9e1ae50767
vandodev <vandoaparecido@hotmail.com>
2025-03-17 07:36:17 -0300

    :rocket: :dollar: :white_check_mark: Criando ranking

--- src/server.ts
+++ src/server.ts
@@ -11,6 +11,7 @@ import {
 import { env } from "./env";
 
 import { accessInviteLinkRoute } from "./routes/access-invite-link-route";
+import { getRankingRoute } from "./routes/get-ranking-route";
 import { getSubscriberInviteClicksRoute } from "./routes/get-subscriber-invite-clicks-route";
 import { getSubscriberInvitesCountRoute } from "./routes/get-subscriber-invites-count-route";
 import { getSubscriberRankingPositionRoute } from "./routes/get-subscriber-ranking-position-route";
@@ -23,6 +24,7 @@ app.setValidatorCompiler(validatorCompiler);
 app.register(getSubscriberInviteClicksRoute);
 app.register(getSubscriberInvitesCountRoute);
 app.register(getSubscriberRankingPositionRoute);
+app.register(getRankingRoute);
 
 app.register(fastifyCors);
 app.register(fastifySwagger, {
